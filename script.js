f7f916ec297680e50a98a3af7adba41496eb42f0
Wemmerson Albuquerque <38667364+WemmersonAlb@users.noreply.github.com>
2022-08-24 13:35:10 -0300

    Criação do Layout da página Sobre e Donate
    Foi desenvolvida a estrutura de conteúdo para a página Sobre e Donate, bem como os designs dos botãos de redes sociais e doação. 
    
    #REVISITAR

---
+++ script.js
@@ -0,0 +1,39 @@
+function claroGit1(){
+    document.getElementById("git1").setAttribute("src", "img/git-claro.png");
+}
+function claroGit2(){
+    document.getElementById("git2").setAttribute("src", "img/git-claro.png");
+}
+function claroGit3(){
+    document.getElementById("git3").setAttribute("src", "img/git-claro.png");
+}
+
+function claroLink1(){
+    document.getElementById("link1").setAttribute("src", "img/link-claro.png");
+}
+function claroLink2(){
+    document.getElementById("link2").setAttribute("src", "img/link-claro.png");
+}
+function claroLink3(){
+    document.getElementById("link3").setAttribute("src", "img/link-claro.png");
+}
+
+function escuroGit1(){
+    document.getElementById("git1").setAttribute("src", "img/git-escuro.png");
+}
+function escuroGit2(){
+    document.getElementById("git2").setAttribute("src", "img/git-escuro.png");
+}
+function escuroGit3(){
+    document.getElementById("git3").setAttribute("src", "img/git-escuro.png");
+}
+
+function escuroLink1(){
+    document.getElementById("link1").setAttribute("src", "img/link-escuro.png");
+}
+function escuroLink2(){
+    document.getElementById("link2").setAttribute("src", "img/link-escuro.png");
+}
+function escuroLink3(){
+    document.getElementById("link3").setAttribute("src", "img/link-escuro.png");
+}
